4e9058f79df57db3b8b779a3bd1401449bc3d9bb
TantaiHaha <78604116+tantaihaha4487@users.noreply.github.com>
2022-06-23 11:25:06 +0700

    commit

---
+++ test2.py
@@ -0,0 +1,16 @@
+import time
+from random import randint as rand
+i = 0
+while i <= 1:
+    number = rand(0, 4)
+    if number == 0 :
+        print("I Love U </3")
+    if number == 1 :
+        print(" I Love U </3")
+    if number == 2 :
+        print("     I Love U </3")
+    if number == 3 :
+        print("         I Love U </3")
+    if number == 4 :
+        print("             I Love U </3")
+    time.sleep(0.5)
